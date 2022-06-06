dd57441a6ec0821c5b4e6c3d6fff93cbd0ff941b
keftylador <jeremiahadegbule@gmail.com>
2022-06-06 13:28:40 -0700

    dd

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/** main - writing a program that with proper grammar, but the outcome is a 
+ * piece of art, 
+ * return: program should return to 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
