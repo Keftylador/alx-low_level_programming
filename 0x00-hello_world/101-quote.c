a6780a6f1285f0747bc634808e4537fc177cc312
keftylador <jeremiahadegbule@gmail.com>
2022-06-06 15:02:53 -0700

    d

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,6 @@
 
 int main(void)
 {
-	write(2, "and that piece of art is very useful - Dora Kopar, 2015-10-19\
-			n", 58);
+	write(2, "and that piece of art is very useful - Dora Kopar, 2015-10-19\n", 58);
 	return (1);
 }
