cce20681fda98692079047e416eede8ab5d62ddc
keftylador <jeremiahadegbule@gmail.com>
2022-06-06 14:54:17 -0700

    f

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Prints out the last part of a quote in the standard error
+ * Return: to 1 if successful
+*/
+
+int main(void)
+{
+	write(2, "and that piece of art is very useful - Dora Kopar, 2015-10-19\
+			n", 58);
+	return (1);
+}
