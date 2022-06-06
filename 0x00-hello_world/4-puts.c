53e55ee588e53b21bf6da168cec14599001e7456
keftylador <jeremiahadegbule@gmail.com>
2022-06-06 13:08:33 -0700

    f

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+/**
+ * main - fuction that prints 'Programming is like building a multilingual
+ * puzzle
+ * Return: gives a value 0 when main function runs successfully else none 0
+*/
+ 
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
